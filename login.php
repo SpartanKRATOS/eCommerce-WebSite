1c64766ec0b55421bb1d698e7bde6c32c1bb3cb4
SpartanKRATOS <sisomatrej@gmail.com>
2021-06-06 19:11:24 +0100

    login.php style.css

---
+++ login.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width , initial-scale=1.0">
+        <title>Login</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="blueBg"></div>
+        </div>
+    </body>
+</html>
