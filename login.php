d67a30f109c25cdf569e5e3f0e850d0cf2a76b32
SpartanKRATOS <sisomatrej@gmail.com>
2021-06-08 20:52:47 +0100

    register php

--- login.php
+++ login.php
@@ -53,6 +53,42 @@ if(isset($_POST['submit'])){
 	// nous avons fermé la connexion à DB
 	mysqli_close($connect);
 }
+
+if(isset($_POST['register'])){
+
+	// importer la connexion DB à partir du fichier connexion.php
+	require 'php/connexion.php';
+	
+	// obtenir les valeurs des champs du formulaire de connexion et les stocker dans des variables
+
+	// mysqli_real_escape_string(para1,para2) : juste pour échapper à un caractère pour sécuriser notre base de données de SQL Injection, para1: nous collons le paramètre du fichier connexion.php appelé $ connect, para2: la valeur du champ
+	
+	// strtolwer(): changer le nom d'utilisateur en minuscules parce que le nom d'utilisateur déposé ne doit pas être sensible à la casse, par exemple: si écrire nassim ou Nassim doit être la même chose, alors je le fais pour le comparer avec celui de notre base de données qui est lui-même en minuscules 
+	
+	$usrname = mysqli_real_escape_string($connect,$_POST['usrname']);
+    $firstname = mysqli_real_escape_string($connect,$_POST['firstname']);
+    $lastname = mysqli_real_escape_string($connect,$_POST['lastname']);
+    $email = mysqli_real_escape_string($connect,$_POST['email']);
+	$usrname=trim(strtolower($usrname));
+    $firstname=trim(strtolower($firstname));
+    $lastname=trim(strtolower($lastname));// trim pour supprimer tout espace indésirable au premier et au dernier des mots;
+	$psswd = mysqli_real_escape_string($connect,$_POST['psswd']);
+    $confpsswd = mysqli_real_escape_string($connect,$_POST['confpsswd']);
+	
+	// on vérifie d'abord si les champs sont vides
+	if(empty($usrname) || empty($firstname) || empty($lastname) || empty($email) || empty($psswd) || empty($confpsswd)){
+		$error="Please fill all the inputs";
+	}else{
+		
+		$result= $connect->query("INSERT INTO users(fname, lname, login, password) VALUES('{$firstname}','{$lastname}','{$usrname}','{$psswd}');") or die(print_r($bdd->errorInfo()));;
+		
+		        session_start();
+				$_SESSION[$usrname] = "ok";
+				header("location:php/profile.php");
+	}
+	// nous avons fermé la connexion à DB
+	mysqli_close($connect);
+}
 ?>
 
 <?php
@@ -108,27 +144,37 @@ include 'inc.php/html_body.php';
                     <form method="POST" autocomplete="off">
                         <h3>Sign Up</h3>
                         <div class="form-row">
-                            <input class="inp" id="username" type="text" required placeholder="Enter your username">
+                            <input class="inp" id="firstname" type="text" required placeholder="Enter your firstame" name="firstname">
+                            <label class="placehold" for="firstname">Enter your <b>FirstName</b> <small></small></label>
+                            <div class="underline"></div>
+                        </div>
+                        <div class="form-row">
+                            <input class="inp" id="lastname" type="text" required placeholder="Enter your lastname" name="lastname">
+                            <label class="placehold" for="lastname">Enter your <b>LastName</b> <small></small></label>
+                            <div class="underline"></div>
+                        </div>
+                        <div class="form-row">
+                            <input class="inp" id="username" type="text" required placeholder="Enter your username" name="usrname">
                             <label class="placehold" for="username">Enter your <b>Username</b> <small></small></label>
                             <div class="underline"></div>
                         </div>
                         <div class="form-row">
-                            <input class="inp" id="email" type="text" required placeholder="Enter your Email Address">
+                            <input class="inp" id="email" type="text" required placeholder="Enter your Email Address" name="email">
                             <label class="placehold" for="email">Enter your <b>Email Address</b> <small></small></label>
                             <div class="underline"></div>
                         </div>
                         <div class="form-row">
-                            <input class="inp" id="password" type="password" required placeholder="Enter your Password">
+                            <input class="inp" id="password" type="password" required placeholder="Enter your Password" name="psswd">
                             <label class="placehold" for="password">Enter your <b>Password</b> <small></small></label>
                             <div class="underline"></div>
                         </div>
                         <div class="form-row">
-                            <input class="inp" id="conf" type="password" required placeholder="Confirm your Password">
+                            <input class="inp" id="conf" type="password" required placeholder="Confirm your Password" name="confpsswd">
                             <label class="placehold" for="conf">Confirm your <b>Password</b> <small></small></label>
                             <div class="underline"></div>
                         </div>
                             </br>
-                        <input type="submit" value="Register">
+                        <input type="submit" name="register" value="Register">
                     </form>
                 </div>
             </div>
