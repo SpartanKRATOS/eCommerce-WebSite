06a19bb6e51a3cca50da19ce4a673c53febc72ea
AHBOUCH Amine <ahbouch.amine@hotmail.com>
2021-06-15 18:05:02 +0000

    removed 3308

--- functions.php
+++ functions.php
@@ -1,28 +1,29 @@
 <?php
 
-function pdo_connect_mysql() {
+function pdo_connect_mysql()
+{
     // Update the details below with your MySQL details
-    $DATABASE_HOST = 'localhost:3308';
+    $DATABASE_HOST = 'localhost';
     $DATABASE_USER = 'root';
     $DATABASE_PASS = '';
     $DATABASE_NAME = 'store';
 
     try {
-       
-    	return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
-        
+
+        return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
     } catch (PDOException $exception) {
-    	// If there is an error with the connection, stop the script and display the error.
-    	exit('Failed to connect to database!');
+        // If there is an error with the connection, stop the script and display the error.
+        exit('Failed to connect to database!');
     }
 }
 // Template header, feel free to customize this
-function template_header($title) {
-  
+function template_header($title)
+{
+
     //Get the amount of items in the shopping cart, this will be displayed in the header
     $num_items_in_cart = isset($_SESSION['cart']) ? count($_SESSION['cart']) : 0;
-    
-echo <<<EOT
+
+    echo <<<EOT
 
 
 <!DOCTYPE html>
@@ -56,9 +57,10 @@ echo <<<EOT
 EOT;
 }
 // Template footer
-function template_footer() {
-$year = date('Y');
-echo <<<EOT
+function template_footer()
+{
+    $year = date('Y');
+    echo <<<EOT
         </main>
         <footer>
             <div class="content-wrapper">
