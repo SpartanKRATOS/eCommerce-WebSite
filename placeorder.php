4c6e551c2673656918ce37d917a37b71ad33e61d
SpartanKRATOS <sisomatrej@gmail.com>
2021-06-21 12:17:24 +0100

    done

--- placeorder.php
+++ placeorder.php
@@ -1,5 +1,17 @@
 <?php
 session_start();
+include 'inc.php/html_body.php';
+
+if (isset($_SESSION['id']) || isset($_SESSION['type'])){
+    $iddd = $_SESSION['id'];
+    $type = $_SESSION['type'];
+    $num_items_in_cart = isset($_SESSION['cart']) ? count($_SESSION['cart']) : 0;
+  
+  }else{
+    $iddd = "";
+    $type = "";
+  
+  }
 ?>
 
 <!DOCTYPE html>
@@ -8,43 +20,31 @@ session_start();
 <head>
     <meta charset="utf-8">
     <title>Completed Order!</title>
-    <link href="css/cart.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="old_css/all.css">
+
+    <link rel="stylesheet" type="text/css" href="homepage.css">
+    <link rel="stylesheet" type="text/css" href="header.css">
+    <link rel="stylesheet" type="text/css" href="ftr.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+    <script src="https://kit.fontawesome.com/e1af7c97bd.js" crossorigin="anonymous"></script>
+
 </head>
 
 <body>
-    <header>
-        <div class="content-wrapper">
-            <h1>Gaming Shop</h1>
-            <nav>
-                <a href="index.php">Home</a>
-                <a href="index.php?page=products">Products</a>
-                <a href="index.php?page=profile">Profile</a>
-                <?php
-                if (empty($_SESSION['id'])) { ?>
-                    <a href="index.php?page=login">Login</a>
-                    <?php
-                } else {
+<?php
+    //$iddd = $_SESSION['id'];
+    //$type = $_SESSION['type'];
+    if (empty($iddd)) {
+      $num_items_in_cart = isset($_SESSION['cart']) ? count($_SESSION['cart']) : 0;
 
-                    if (!empty($_SESSION['id'])) {
+        sub_headers($num_items_in_cart);
+    }else
+    if ($type != 'admin') {
+        sub_headers1($iddd, $type,$num_items_in_cart);
+    }else{admin_headers($iddd, $type,$num_items_in_cart);}
+    
 
-                    ?>
-                        <a href="index.php?page=logout">Logout</a>
-                <?php
-
-                    }
-                }
-                ?>
-            </nav>
-            <div class="link-icons">
-                <a href="index.php?page=cart">
-                    <i class="fas fa-shopping-cart"></i>
-                    <?php $num_items_in_cart = isset($_SESSION['cart']) ? count($_SESSION['cart']) : 0; ?>
-                    <span><?= $num_items_in_cart ?></span>
-                </a>
-            </div>
-        </div>
-    </header>
+    ?>
     <main>
 
         <div class="placeorder content-wrapper">
@@ -52,4 +52,7 @@ session_start();
             <p>Thank you for ordering with us, we'll contact you by email with your order details.</p>
         </div>
 
-        <?= template_footer() ?>
+        </main>
+</body>
+<?php sub_footer(); ?>
+</html>
